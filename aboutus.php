d4b0d1d730cf20985c862b42124425f51ccac1b2
mukamieee <sharon.murithi@strathmore.edu>
2024-07-12 10:07:27 +0300

    removed nav block and replaced with one line

--- aboutus.php
+++ aboutus.php
@@ -8,21 +8,7 @@
     <link rel="stylesheet" href="CSS/dropdown.css">
 </head>
 <body>
-  <nav class="navbar">
-    <a href="#" class="logo">Sha's Adornments</a>
-    <ul class="navlinks">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="aboutus.php">About</a></li>
-        <li><a href="shop.php">Shop</a></li>
-        <li><a href="custom.php">Custom</a></li>
-        <li><a href="testimonials.php">Testimonials</a></li>
-        <li><a href="policies.php">Policies</a></li>
-        <li><a href="FAQs.php">FAQs</a></li>
-    </ul>
-    <div class="right">
-        <a href="signup.php">Sign Up</a>
-        <a href="login.php">Sign In</a>
-    </div>
+<?php include_once ("templates/nav.php"); ?>
 </nav>
 
     <h1>Made with love since 2024</h1>
