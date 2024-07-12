d30d4615b8f17b920fe2443d807a20195778dc68
mukamieee <sharon.murithi@strathmore.edu>
2024-07-12 09:34:17 +0300

    inserted data in tables

--- SQL/db.sql
+++ SQL/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 12, 2024 at 02:50 AM
+-- Generation Time: Jul 12, 2024 at 08:33 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -52,6 +52,34 @@ INSERT INTO `genders` (`gendrId`, `gender`, `datecreated`, `dateupdated`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
+  `productId` int(10) NOT NULL AUTO_INCREMENT,
+  `productName` varchar(50) NOT NULL,
+  `Description` text NOT NULL,
+  `Price` bigint(20) NOT NULL,
+  `Material` varchar(50) NOT NULL,
+  `Stock` bigint(20) NOT NULL DEFAULT 0,
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`productId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`productId`, `productName`, `Description`, `Price`, `Material`, `Stock`, `createdAt`) VALUES
+(1, 'Necklace', 'Simple With diamonds ', 3000, 'Diamonds and Gold', 100, '2024-07-12 09:25:07'),
+(2, 'Rings', 'Simple and Gothic', 1239, 'Alloy', 28, '2024-07-12 09:25:07'),
+(3, 'Earrings', 'Pure glimmering gold', 4000, 'Gold', 99, '2024-07-12 09:25:07'),
+(4, 'Bracelets', 'Charm silver bracelets', 150, 'Silver Metal', 2000, '2024-07-12 09:25:07');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `roles`
 --
 
@@ -96,6 +124,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userID`, `fullname`, `email`, `password`, `username`, `roleId`, `genderId`, `datecreated`, `dateupdated`) VALUES
+(1, 'Lewa Kadzo', 'kadzo@example.com', 'abc123', 'klewa', 1, 1, '2024-07-12 09:30:11', '2024-07-12 09:30:11'),
+(2, 'Nicole Ndila', 'ndila@example.com', 'efg123', 'nndila', 2, 1, '2024-07-12 09:30:11', '2024-07-12 09:31:39'),
+(3, 'Joy Njeru', 'joy@example.com', 'hij123', 'jnjeru', 3, 1, '2024-07-12 09:30:11', '2024-07-12 09:31:39'),
+(4, 'Jahleel Mollel', 'jahleel@example.com', 'lmnop1234', 'mjahleel', 4, 2, '2024-07-12 09:30:11', '2024-07-12 09:31:39');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
