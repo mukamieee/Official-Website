e2d77db4b3d0127f1391da06a2af4c0afcc75f32
mukamieee <sharon.murithi@strathmore.edu>
2024-07-12 10:09:30 +0300

    replaced nav block

---
+++ templates/nav.php
@@ -0,0 +1,16 @@
+<nav class="navbar">
+    <a href="#" class="logo">Sha's Adornments</a>
+    <ul class="navlinks">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="aboutus.php">About</a></li>
+        <li><a href="shop.php">Shop</a></li>
+        <li><a href="custom.php">Custom</a></li>
+        <li><a href="testimonials.php">Testimonials</a></li>
+        <li><a href="policies.php">Policies</a></li>
+        <li><a href="FAQs.php">FAQs</a></li>
+    </ul>
+    <div class="right">
+        <a href="signup.php">Sign Up</a>
+        <a href="login.php">Sign In</a>
+    </div>
+</nav>
