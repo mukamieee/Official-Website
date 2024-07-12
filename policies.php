275c962c5d5a199e4ed8966397bef181b185daf6
mukamieee <sharon.murithi@strathmore.edu>
2024-07-12 10:09:00 +0300

    replaced nav block

--- policies.php
+++ policies.php
@@ -7,22 +7,7 @@
     <link rel="stylesheet" href="CSS/navbar.css">
 </head>
 <body>
-    <nav class="navbar">
-        <a href="#" class="logo">Sha's Adornments</a>
-        <ul class="navlinks">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="aboutus.php">About</a></li>
-            <li><a href="shop.php">Shop</a></li>
-            <li><a href="custom.php">Custom</a></li>
-            <li><a href="testimonials.php">Testimonials</a></li>
-            <li><a href="policies.php">Policies</a></li>
-            <li><a href="FAQs.php">FAQs</a></li>
-        </ul>
-        <div class="right">
-            <a href="signup.php">Sign Up</a>
-            <a href="login.php">Sign In</a>
-        </div>
-    </nav>
+    <?php include_once ("templates/nav.php"); ?>
     <h2>Return & Exchange Policy</h2>
     <!-- add bullet points  -->
     <p>We accept returns within 30 days of purchase. To initiate a return, please contact our customer service team. Items must be unworn and in their original packaging.
